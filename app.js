3346239fc86a762122bcaf5a0221d1ea8aea16bb
Sebastian Schwarz <schwarz.duscheleit@hotmail.de>
2023-10-07 16:55:47 +0200

    sol

--- app.js
+++ app.js
@@ -10,16 +10,58 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // middleware: before a client acesses the server
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', (req, res) => {
+
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    //console.log(beersFromApi);
+    res.render('beers', beersFromApi)
+  })
+  .catch(error => console.log(error));
+
+  
+});
+
+app.get('/random-beer', async (req, res) => {
+
+  try {
+  const response = await punkAPI.getRandom();
+  res.render('random-beer', response[0]);
+  
+  }
+  catch(error){
+
+    console.log(error);
+}
+});
+
+// Assuming you have a route like this for displaying detailed information about a beer
+app.get('/beers/:beerId', (req, res) => {
+  const beerId = req.params.beerId;
+  
+  // Retrieve the detailed information about the specific beer using the beerId
+  punkAPI.getBeer(beerId)
+  .then((beer) => {
+    console.log(beer)
+    res.render('beerDetails', beer[0]);
+  })
+  
+  // Render the beer details page and pass the beer details to it
+  
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
